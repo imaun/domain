1a246fff9d26f9616fb90b80ae5e8e2dca88573f
imun <imun22@gmail.com>
2022-07-20 12:42:52 +0430

    Add DomainException

---
+++ src/Domain/Base/DomainException.cs
@@ -0,0 +1,15 @@
+namespace iman.Domain;
+
+public class DomainException : Exception
+{
+    public DomainException() : base()
+    {
+    }
+    
+    public DomainException(string message): base(message) { }
+
+    public DomainException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
