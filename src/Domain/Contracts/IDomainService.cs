7da66e014ac979d6d3b649c2679f10ac5b1c05b8
imun <imun22@gmail.com>
2022-07-20 15:18:04 +0430

    Add DomainService comments

--- src/Domain/Contracts/IDomainService.cs
+++ src/Domain/Contracts/IDomainService.cs
@@ -1,10 +1,15 @@
 namespace iman.Domain;
 
-public interface IDomainService<T> : IDomainService where T : IAggregateRoot
+/// <summary>
+/// Domain Service for <see cref="IAggregateRoot"/> or <see cref="IEntity"/> models.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IDomainService<T> : IDomainService where T : IAggregateRoot, IEntity
 {
     
 }
 
+/// <inheritdoc cref="IDomainService{T}"/> 
 public interface IDomainService
 {
     
